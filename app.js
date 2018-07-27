7e164b6b9b19e280d734c6efcfbe9d7b56d2801d
Kurt <home@Kurts-MBP.hsd1.fl.comcast.net>
2018-07-27 09:04:42 -0400

    latest

--- app.js
+++ app.js
@@ -123,6 +123,7 @@ app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawes
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.post('/', homeController.postTrip);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
--- controllers/home.js
+++ controllers/home.js
@@ -7,3 +7,45 @@ exports.index = (req, res) => {
     title: 'Home'
   });
 };
+
+exports.postTrip = (req, res) => {
+return res.redirect('/contact');
+  let fromName;
+  let fromEmail;
+  if (!req.user) {
+    req.assert('name', 'Name cannot be blank').notEmpty();
+    req.assert('email', 'Email is not valid').isEmail();
+  }
+  req.assert('message', 'Message cannot be blank').notEmpty();
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/contact');
+  }
+
+  if (!req.user) {
+    fromName = req.body.name;
+    fromEmail = req.body.email;
+  } else {
+    fromName = req.user.profile.name || '';
+    fromEmail = req.user.email;
+  }
+
+  const mailOptions = {
+    to: 'your@email.com',
+    from: `${fromName} <${fromEmail}>`,
+    subject: 'Contact Form | Hackathon Starter',
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      req.flash('errors', { msg: err.message });
+      return res.redirect('/contact');
+    }
+    req.flash('success', { msg: 'Email has been sent successfully!' });
+    res.redirect('/contact');
+  });
+};
